cfd145b747e9e869f36abb6527b03435046d4a22
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2019-03-22 18:33:11 -0300

    Validação de inputs
    Validação para que os inputs não sejam enviados em branco

--- js/validacao.js
+++ js/validacao.js
@@ -3,3 +3,14 @@
 Futuras validações que faltaram implementar
 
 */
+
+function valida() {
+	var nome = document.getElementById('campo-nome');
+	var peso = document.getElementById('campo-peso');
+	var altura = document.getElementById('campo-altura');
+
+	if (nome.value == "" || peso.value == "" || altura.value == "") {
+		alert("Preencha todos os campos !");
+		window.location.reload(true);
+	}
+}
